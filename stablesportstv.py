7ee5d72286d34e27668161036f69056c77d9caf8
stablesports711-hue <stablesports711@gmail.com>
2026-05-17 10:40:15 +0600

    Update stablesportstv.py

--- stablesportstv.py
+++ stablesportstv.py
@@ -31,7 +31,7 @@ https://premierleagpl23.akamaized.net/hls/live/2107108/tapmad-P2s6L_FiN@L-UrU/ma
 http://10.10.10.2/live/fnf004/index.m3u8
 
 #EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://d34080pnh6e62j.cloudfront.net/images/channels/mobile_large/17780696481020x576.jpg",Bangladesh Vs Pakistan
-https://crichd.johirxofficial.workers.dev/live/ptvpk.m3u8
+http://main.epgmaker.com/live/y49sz6KMQs/6115263489/498.ts
 
 #EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://i.postimg.cc/fLh0bmsF/images-(2).jpg",IPL [2026]|HD
 http://premimum.online/live/jen12345/jen54321/705142.ts
@@ -258,7 +258,7 @@ http://198.195.239.50:8095/Tsports/tracks-v1a1/mono.m3u8
 http://198.195.239.50:8095/Eurosport/index.m3u8
 
 #EXTINF:-1 tvg-logo="https://ev-img-cdn-lb.shoq.com.pk/prd-peg-data/default/images/logos/live/PTV-Sports.png" group-title="Sports",PTV Sports
-http://10.10.10.2/live/fnf002/index.m3u8
+http://main.epgmaker.com/live/y49sz6KMQs/6115263489/498.ts
 
 #EXTINF:-1 tvg-id="ext" tvg-name="SONY SPORTS 1 HD" tvg-logo="https://raw.githubusercontent.com/AsimDipto/Logo-box/refs/heads/main/India/sony-ten-1-hd-in.png" group-title="Sports",SONY SPORTS 1HD
 http://premimum.online/live/jen12345/jen54321/154.ts
