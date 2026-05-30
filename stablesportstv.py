1e9f8966d7f0e1e8a5d30753c113781625187aa6
stablesports711-hue <stablesports711@gmail.com>
2026-05-30 15:08:29 +0600

    Update stablesportstv.py

--- stablesportstv.py
+++ stablesportstv.py
@@ -39,6 +39,13 @@ http://198.195.239.50:8095/StarSports2/tracks-v1a1/mono.m3u8
 https://tvsen5.aynaott.com/willowhd/index.m3u8
 
 #EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://i.postimg.cc/fLh0bmsF/images-(2).jpg",IPL [2026]| HD
+
+
+#EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://i.postimg.cc/fLh0bmsF/images-(2).jpg",IPL [2026]| HD
+
+#EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://i.postimg.cc/fLh0bmsF/images-(2).jpg",IPL [2026]| HD
+
+#EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://i.postimg.cc/fLh0bmsF/images-(2).jpg",IPL [2026]| HD
 http://main.epgmaker.com/live/y49sz6KMQs/6115263489/500.ts
 
 #EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://i.postimg.cc/fLh0bmsF/images-(2).jpg",IPL [2026]| HD
