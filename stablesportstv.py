97339d501c9eac524d3d7c958eec74b7a73b00f7
stablesports711-hue <stablesports711@gmail.com>
2026-06-04 09:27:22 +0600

    Update stablesportstv.py

--- stablesportstv.py
+++ stablesportstv.py
@@ -15,6 +15,9 @@ sources = [
 custom_channels = """
 #EXTM3U
 
+#EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://i.postimg.cc/c18hMmZL/F5MBOPJ3X5A35EJX2HXMUUFQNU.jpg",FIFA World Cup 2026
+http://10.10.10.2/live/fnf004/index.m3u8
+
 #EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://i.postimg.cc/c18hMmZL/F5MBOPJ3X5A35EJX2HXMUUFQNU.jpg",FIFA World Cup 2026 🇦🇷[coming]
 http://cdn.tv-rds.workers.dev/TYCSPT.m3u8
 #https://amg26268-amg26268c14-freelivesports-emea-10267.playouts.now.amagi.tv/ts-us-e2-n2/playlist/amg26268-sportsstudio-tycsports-freelivesportsemea/playlist.m3u8
