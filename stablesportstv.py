2141bab61e3c205b8ec46995720f4f598c006fcb
stablesports711-hue <stablesports711@gmail.com>
2026-06-27 17:15:03 +0600

    Update stablesportstv.py

--- stablesportstv.py
+++ stablesportstv.py
@@ -46,11 +46,11 @@ http://198.195.239.50:8095/btv/index.m3u8
 http://198.195.239.50:8095/somoyTv/index.m3u8
 
 
-#EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://i.postimg.cc/4NWsyqfM/20260607-190818.jpg",FIFA World Cup 2026🅰️
+#EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://i.postimg.cc/4NWsyqfM/20260607-190818.jpg",FIFA World Cup 2026 HD
 http://198.195.239.50:8095/tsports/index.m3u8
 
-#EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://i.postimg.cc/4NWsyqfM/20260607-190818.jpg",FIFA World Cup 2026🅱️
-http://172.17.50.112:84/ant1001/index.m3u8
+#EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://i.postimg.cc/4NWsyqfM/20260607-190818.jpg",FIFA World Cup 2026
+http://premiumtvs.space/live/jen12345/Jen54321/113069.ts
 
 #EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://i.postimg.cc/7LW7Jnw0/20260612-094836.jpg",🅰️FIFA World Cup 2026(Tapmad)PK only
 https://premierleagpl23.akamaized.net/hls/live/2107108/tapmad-P2s6L_FiN@L-UrU/master.m3u8
