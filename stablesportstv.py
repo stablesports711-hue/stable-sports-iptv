c8963cee0f9c0a93748737e5f9d27c9bf0ec970c
stablesports711-hue <stablesports711@gmail.com>
2026-07-04 16:12:15 +0600

    Update streaming URL for FIFA World Cup 2026

--- stablesportstv.py
+++ stablesportstv.py
@@ -125,7 +125,7 @@ http://198.195.239.50:8095/ptv/index.m3u8
 http://lefanten.com:8080/live/xuxDj2EU/jtxWZHR4nzquT/1434609.m3u8
 
 #EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://i.postimg.cc/1zPBwRBH/20260704-160037.jpg",FIFA World Cup 2026 UC
-http://198.195.239.50:8095/ptv/index.m3u8
+http://lefanten.com:8080/live/xuxDj2EU/jtxWZHR4nzquT/1434639.m3u8
 
 #EXTINF:-1 group-title="LIVE SPORTS" tvg-logo="https://i.postimg.cc/1zPBwRBH/20260704-160037.jpg",FIFA World Cup 2026 UC
 http://198.195.239.50:8095/ptv/index.m3u8
