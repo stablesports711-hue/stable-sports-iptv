7d99ce770dce4ce5d376eae37749144ea54af705
stablesports711-hue <stablesports711@gmail.com>
2026-08-16 07:27:50 +0600

    Update stablesportstv.py

--- stablesportstv.py
+++ stablesportstv.py
@@ -16,8 +16,7 @@ sources = [
 
 custom_channels = """#EXTM3U
 #EXTINF:-1 tvg-logo="https://d34080pnh6e62j.cloudfront.net/images/channels/mobile_large/17864294671020x576.jpg" group-title="LIVE SPORTS", Australia vs Bangladesh | Bangladesh Tour of Australia 2026 [S-1]
-https://saseries.akamaized.net/hls/live/2110097/56408643-tapMad/master.m3u8
-
+https://saseries.akamaized.net/hls/live/2110097/2356jkiL-tapmad/master.m3u8
 
 #EXTINF:-1 tvg-logo="https://d34080pnh6e62j.cloudfront.net/images/channels/mobile_large/17864294671020x576.jpg" group-title="LIVE SPORTS", Australia vs Bangladesh | Bangladesh Tour of Australia 2026 [S-2]
 https://raw.githubusercontent.com/stablesports711-hue/stable-sports-movie/refs/heads/main/IPTV/FOX501HD.m3u8
